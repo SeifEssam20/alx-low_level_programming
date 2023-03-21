ac1586dea0a6e2f94a0d9452d1d010cd369d506f
SeifEssam20 <seifessam69209@gmail.com>
2023-03-21 10:19:08 -0700

    a5er mra

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,7 @@
 #include "main.h"
 
-/**
- * print_alphabet - prints the alphabet in lowercase
- */
-void print_alphabet(void)
-{
-char letter;
-
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar(letter);
-}
-_putchar('\n');
-}
-
 int main(void)
 {
 print_alphabet();
-return(0);
+return (0);
 }
