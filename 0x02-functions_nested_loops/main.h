204f2448f27d2d3996231170c795a6dc893438a1
SeifEssam20 <seifessam69209@gmail.com>
2023-03-21 13:59:10 -0700

    ya mosahel

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _abs(int);
 int _isalpha(int c);
 int print_sign(int n);
 int _islower(int c);
