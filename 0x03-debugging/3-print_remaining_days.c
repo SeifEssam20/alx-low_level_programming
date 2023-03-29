90cdc91c887adbb2e89e068831d87f3f810d26a8
SeifEssam20 <seifessam69209@gmail.com>
2023-03-28 21:12:28 -0700

    bdany etf23t

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,30 +6,46 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int i, day_of_year = 0;
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int day_of_year = 0;
+int i;
 
-for (i = 1; i < month; i++)
+if (year % 4 == 0)
 {
-day_of_year += days_in_month[i];
+if (year % 100 == 0)
+{
+if (year % 400 == 0)
+{
+days_in_month[1] = 29;
 }
-day_of_year += day;
-
-if ((year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) &&
-(month > 2 || (month == 2 && day > 29)))
-{
-day_of_year++;
-}
-
-if (month > 12 || day > days_in_month[month])
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 1 : 0) - day_of_year);
+days_in_month[1] = 29;
 }
+}
+
+for (i = 0; i < month - 1; i++)
+{
+day_of_year += days_in_month[i];
+}
+
+day_of_year += day;
+
+if (days_in_month[month - 1] < day)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
+}
+
+if (days_in_month[1] == 29 && month > 2)
+{
+day_of_year--;
+}
+
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", 365 + (days_in_month[1] == 29 ? 1 : 0) - day_of_year);
 }
