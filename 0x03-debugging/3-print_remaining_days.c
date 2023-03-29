f16a985a3b4ce535819bc1d31785d223dd314a9f
SeifEssam20 <seifessam69209@gmail.com>
2023-03-29 04:01:04 -0700

    ....

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,51 +1,54 @@
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * print_remaining_days - Takes a date and prints how many days are left in the year,
+ * taking leap years into account.
+ * @month: The month of the date.
+ * @day: The day of the date.
+ * @year: The year of the date.
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int day_of_year = 0;
+int days_remaining = 0;
 int i;
 
-if (year % 4 == 0)
-{
-if (year % 100 == 0)
-{
-if (year % 400 == 0)
-{
-days_in_month[1] = 29;
-}
-}
-else
-{
-days_in_month[1] = 29;
-}
-}
-
-for (i = 0; i < month - 1; i++)
+// Calculate day of year for non-leap year
+for (i = 1; i < month; i++)
 {
 day_of_year += days_in_month[i];
 }
-
 day_of_year += day;
 
-if (days_in_month[month - 1] < day)
+// Check for leap year and adjust day of year and days in February
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+days_in_month[2] = 29;
+if (month > 2)
+{
+day_of_year++;
+}
+}
+
+// Calculate days remaining in year
+days_remaining = 365 - day_of_year;
+for (i = month + 1; i <= 12; i++)
+{
+days_remaining += days_in_month[i];
+}
+
+// Print results
+if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
 }
-
-if (days_in_month[1] == 29 && month > 2)
+else if (days_remaining == 0)
 {
-day_of_year--;
-}
-
 printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", 365 + (days_in_month[1] == 29 ? 1 : 0) - day_of_year);
+printf("Happy new year!\n");
+}
+else
+{
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", days_remaining);
+}
 }
