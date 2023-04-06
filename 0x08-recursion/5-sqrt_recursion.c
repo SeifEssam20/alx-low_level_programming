f0db19ebf10355302c3dc6fb11226b803109698f
root <seifessam690209@gmail.com>
2023-04-06 06:20:05 -0700

    limbo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to compute the square root of
+ * @n: the number to find the square root of
  *
- * Return: the natural square root of n, or -1 if n has no natural square root
+ * Return: the natural square root of n, or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return -1;
+return( -1);
 if (n == 0 || n == 1)
-return n;
-int res = _sqrt_recursion(n / 4) * 2;
-if (res * res <= n && (res + 1) * (res + 1) > n)
-return res;
-if (res * res > n)
-return _sqrt_recursion(n / 4);
-return _sqrt_recursion(n);
+return (n);
+int res;
+res = _sqrt_recursion(n / 4) * 2;
+if ((res + 1) * (res + 1) <= n && (res + 1) * (res + 1) > 0)
+return (res + 1);
+else
+return (res);
 }
