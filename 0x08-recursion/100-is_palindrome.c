3edf251d05530bbd30d446e03714381be85bbafc
root <seifessam690209@gmail.com>
2023-04-06 06:42:57 -0700

    limbo

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+
+return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
 int len;
