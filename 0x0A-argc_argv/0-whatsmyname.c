e39bcde4392a112e451e12a08b84b086736d340d
SeifEssam20 <seifessam69209@gmail.com>
2023-04-09 07:18:49 +0000

    gcc2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,23 @@
-#include"main.h"
-int main(int argc , char *argv[])
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
 {
- (void) argc ;
- printf("%s\n", argv[0]);
- return(0);
+(void)argc;
+char *str = argv[0];
+
+while (*str)
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+
+return (0);
 }
