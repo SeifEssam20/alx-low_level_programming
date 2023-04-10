1496a7e42a64cfed10645cc16fb831534104942e
SeifEssam20 <seifessam69209@gmail.com>
2023-04-10 03:42:07 +0000

    gcc4

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+  int i, j;
+
+  for (i = 0; i < argc; i++)
+    {
+      for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	  _putchar(argv[i][j]);
+	}
+      _putchar('\n');
+    }
+
+  return (0);
+}
