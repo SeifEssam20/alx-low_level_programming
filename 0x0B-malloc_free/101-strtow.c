b398737078be27703ca7314fc062a929e46386c1
SeifEssam20 <seifessam69209@gmail.com>
2023-04-11 22:48:43 +0000

    mem

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,21 +8,21 @@
  */
 int count_words(char *str)
 {
-  int words = 0, i = 0;
+int words = 0, i = 0;
 
-  while (str[i])
-    {
-      if (str[i] != ' ')
-	{
-	  words++;
-	  while (str[i] && str[i] != ' ')
-	    i++;
-	}
-      else
-	i++;
-    }
+while (str[i])
+{
+if (str[i] != ' ')
+{
+words++;
+while (str[i] && str[i] != ' ')
+i++;
+}
+else
+i++;
+}
 
-  return (words);
+return (words);
 }
 
 /**
@@ -33,40 +33,40 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-  char **words;
-  int i = 0, j, k, len, word_count;
+char **words;
+int i = 0, j, k, len, word_count;
 
-  if (str == NULL || str[0] == '\0')
-    return (NULL);
+if (str == NULL || str[0] == '\0')
+return (NULL);
 
-  word_count = count_words(str);
+word_count = count_words(str);
 
-  words = malloc(sizeof(char *) * (word_count + 1));
-  if (words == NULL)
-    return (NULL);
+words = malloc(sizeof(char *) * (word_count + 1));
+if (words == NULL)
+return (NULL);
 
-  while (i < word_count)
-    {
-      while (*str == ' ')
-	str++;
-      len = 0;
-      while (str[len] != ' ' && str[len] != '\0')
-	len++;
-      words[i] = malloc(sizeof(char) * (len + 1));
-      if (words[i] == NULL)
-	{
-	  for (j = 0; j < i; j++)
-	    free(words[j]);
-	  free(words);
-	  return (NULL);
-	}
-      for (k = 0; k < len; k++)
-	words[i][k] = *(str++);
-      words[i][k] = '\0';
-      i++;
-    }
+while (i < word_count)
+{
+while (*str == ' ')
+str++;
+len = 0;
+while (str[len] != ' ' && str[len] != '\0')
+len++;
+words[i] = malloc(sizeof(char) * (len + 1));
+if (words[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(words[j]);
+free(words);
+return (NULL);
+}
+for (k = 0; k < len; k++)
+words[i][k] = *(str++);
+words[i][k] = '\0';
+i++;
+}
 
-  words[i] = NULL;
+words[i] = NULL;
 
-  return (words);
+return (words);
 }
