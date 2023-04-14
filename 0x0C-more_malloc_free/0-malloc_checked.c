84657dc46a3803294115a33c003b8d68666b94df
SeifEssam20 <seifessam69209@gmail.com>
2023-04-14 05:42:19 +0000

    mem

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The amount of memory to allocate
+ *
+ * Return: A pointer to the allocated memory
+ *         If malloc fails, the malloc_checked function terminates with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+exit(98);
+
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+memory is still scary
