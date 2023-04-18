036557f405b1199191166a9cb46d77cf905f1819
SeifEssam20 <seifessam69209@gmail.com>
2023-04-18 09:42:41 +0200

    adham

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog_t;
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
