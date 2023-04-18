697402b80979567d3252cb6f2da4182f161f929a
SeifEssam20 <seifessam69209@gmail.com>
2023-04-18 09:52:03 +0200

    adham

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs
+ * @d: Pointer to struct dog
+ *
+ * Return: Void
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+
+free(d->name);
+free(d->owner);
+free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,5 @@ char *owner;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+void free_dog(dog_t *d);
 #endif /* DOG_H */
