f1a0f1340037d358bda85713d5902647f594c6a5
SeifEssam20 <seifessam69209@gmail.com>
2023-04-21 01:03:17 +0200

    varg

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,9 +9,7 @@ void print_all(const char * const format, ...)
 va_list args;
 unsigned int i = 0;
 char *str;
-
 va_start(args, format);
-
 while (format && format[i])
 {
 switch (format[i])
@@ -28,24 +26,17 @@ break;
 case 's':
 str = va_arg(args, char *);
 if (str == NULL)
-{
 str = "(nil)";
-}
 printf("%s", str);
 break;
 default:
 i++;
 continue;
 }
-
 if (format[i + 1])
-{
 printf(", ");
-}
-
 i++;
 }
-
 va_end(args);
 printf("\n");
 }
