f05e0fbe5e086262698a9c83f94f49f981435a7c
SeifEssam20 <seifessam69209@gmail.com>
2023-04-27 09:57:49 +0200

    ......

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
