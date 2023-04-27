bd6beea50779e3d8ec8daae564828dbe58482c5c
SeifEssam20 <seifessam69209@gmail.com>
2023-04-27 11:36:47 +0200

    ......

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,19 +26,20 @@ free(new_node);
 return (NULL);
 }
 
+new_node->len = strlen(str);
 new_node->next = NULL;
 
 if (*head == NULL)
 {
 *head = new_node;
-return (new_node);
 }
-
+else
+{
 temp = *head;
 while (temp->next != NULL)
 temp = temp->next;
-
 temp->next = new_node;
+}
 
 return (new_node);
 }
