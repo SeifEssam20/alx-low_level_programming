7460781cdce004004bf2ffc2147f3a7aaf5f13c4
SeifEssam20 <seifessam69209@gmail.com>
2023-04-29 14:07:10 +0200

    ..............

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,40 +10,40 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *tmp, *check;
-    const listint_t *visited[2048] = {NULL};
-    size_t index;
+size_t count = 0;
+const listint_t *tmp, *check;
+const listint_t *visited[2048] = {NULL};
+size_t index;
 
-    if (head == NULL)
-        exit(98);
+if (head == NULL)
+exit(98);
 
-    tmp = head;
-    check = head;
+tmp = head;
+check = head;
 
-    while (tmp != NULL && count <= 2048)
-    {
-        printf("[%p] %d\n", (void *)tmp, tmp->n);
-        count++;
+while (tmp != NULL && count <= 2048)
+{
+printf("[%p] %d\n", (void *)tmp, tmp->n);
+count++;
 
-        /* Check if the node has already been visited */
-        index = ((size_t)tmp) % 2048;
-        if (visited[index] == tmp)
-        {
-            printf("-> [%p] %d\n", (void *)tmp, tmp->n);
-            exit(98);
-        }
+/* Check if the node has already been visited */
+index = ((size_t)tmp) % 2048;
+if (visited[index] == tmp)
+{
+printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+exit(98);
+}
 
-        visited[index] = tmp;
+visited[index] = tmp;
 
-        tmp = tmp->next;
+tmp = tmp->next;
 
-        if (tmp == NULL)
-        {
-            check = head;
-            tmp = check;
-        }
-    }
+if (tmp == NULL)
+{
+check = head;
+tmp = check;
+}
+}
 
-    return (count);
+return (count);
 }
