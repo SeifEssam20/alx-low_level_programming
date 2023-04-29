00cdc9967bd99c5a60652682328ecb17bb8b77dd
SeifEssam20 <seifessam69209@gmail.com>
2023-04-29 14:05:47 +0200

    ...........................

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * print_listint_safe - prints a linked list, handling loops
@@ -7,32 +10,40 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
-const listint_t *tmp, *check;
+    size_t count = 0;
+    const listint_t *tmp, *check;
+    const listint_t *visited[2048] = {NULL};
+    size_t index;
 
-if (head == NULL)
-exit(98);
+    if (head == NULL)
+        exit(98);
 
-tmp = head;
-check = head;
+    tmp = head;
+    check = head;
 
-while (tmp != NULL)
-{
-printf("[%p] %d\n", (void *)tmp, tmp->n);
-tmp = tmp->next;
-count++;
+    while (tmp != NULL && count <= 2048)
+    {
+        printf("[%p] %d\n", (void *)tmp, tmp->n);
+        count++;
 
-if (tmp >= check)
-{
-printf("-> [%p] %d\n", (void *)tmp, tmp->n);
-exit(98);
-}
+        /* Check if the node has already been visited */
+        index = ((size_t)tmp) % 2048;
+        if (visited[index] == tmp)
+        {
+            printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+            exit(98);
+        }
 
-if (tmp == NULL)
-{
-check = head;
-tmp = check;
-}
-}
-return (count);
+        visited[index] = tmp;
+
+        tmp = tmp->next;
+
+        if (tmp == NULL)
+        {
+            check = head;
+            tmp = check;
+        }
+    }
+
+    return (count);
 }
